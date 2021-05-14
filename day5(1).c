2dffb85aba524cbf1e855ecc77eb85225d38eed6
akhilrajb <77284849+akhilrajb@users.noreply.github.com>
2021-05-14 15:07:20 +0530

    Create day5(1).c

---
+++ day5(1).c
@@ -0,0 +1,12 @@
+//Write a C Program to verify if a given year is a leap year or no
+#include <stdio.h>
+int main()
+{
+  int year; 
+  scanf("%d", &year); 
+  if (( year%400 == 0)|| (( year%4 == 0 ) &&( year%100 != 0)))
+      printf("Yes");
+  else
+      printf("No");  
+   return 0;
+}
