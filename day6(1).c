d0da373c1c3c0603edca06e6d2b1fd34a0ca4321
akhilrajb <77284849+akhilrajb@users.noreply.github.com>
2021-05-15 19:48:04 +0530

    Create day6(1).c

---
+++ day6(1).c
@@ -0,0 +1,26 @@
+//n a sequence of numbers from 1 - 100, replace the numbers divisble by 3 with "FIZZ", replace thenumbers 
+//divisble by 5 with "BUZZ" and replace the numbers divisble by 15 with "FIZZBUZZ" and print the result.
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i=1; i<=100; i++)
+    {
+     
+        if (i%15 == 0)       
+            printf ("FIZZBUZZ\t");   
+   
+        else if ((i%3) == 0)   
+            printf("FIZZ\t");                
+     
+        else if ((i%5) == 0)                      
+            printf("BUZZ\t");                
+     
+        else            
+            printf("%d\t", i);                
+ 
+    }
+ 
+    return 0;
+}
