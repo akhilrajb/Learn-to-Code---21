ce5b18dc6b3ca1c996d0cc83c1432994932c75fb
akhilrajb <77284849+akhilrajb@users.noreply.github.com>
2021-05-17 18:46:58 +0530

    Create day(7).c

---
+++ day(7).c
@@ -0,0 +1,30 @@
+//WACP to determine if a triangle is valid from the given sides.
+#include<stdio.h>
+int main()
+{
+    int side1, side2, side3;
+       scanf("%d%d%d", &side1, &side2, &side3);
+       if(side1 + side2 > side3)
+        {
+      if(side2 + side3 > side1)
+      {
+      if(side1 + side3 > side2)
+       {
+        printf("Yes");    
+     }
+     else
+     {
+      printf("No");
+     }              
+     }
+     else
+     {
+      printf("No");
+     }  
+         }
+            else
+         {
+      printf("No");
+     }  
+     return 0;
+ }
